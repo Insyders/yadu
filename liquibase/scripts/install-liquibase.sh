e4ccafe88f5a7b0b751da086f32ab4725a5028e5
Tommy Gingras <tommy@halfserious.com>
2021-06-07 16:36:26 -0400

    fix: rollback config

--- liquibase/scripts/install-liquibase.sh
+++ liquibase/scripts/install-liquibase.sh
@@ -8,15 +8,12 @@ rm -f ./mysql-connector-java-8.0.24.jar
 
 echo "Downloading liquibase V4.3.5 from Half Serious public S3 Bucket"
 
-# curl --silent https://yadu-public.s3.ca-central-1.amazonaws.com/liquibase-4.3.5.zip -o ./liquibase-4.3.5.zip
-curl https://yadu-public.s3.ca-central-1.amazonaws.com/liquibase-4.3.5.zip -o ./liquibase-4.3.5.zip
-# unzip -q -o liquibase-4.3.5.zip
-unzip -o liquibase-4.3.5.zip
+curl --silent https://yadu-public.s3.ca-central-1.amazonaws.com/liquibase-4.3.5.zip -o ./liquibase-4.3.5.zip
+unzip -q -o liquibase-4.3.5.zi
 rm -rf liquibase-4.3.5.zip
 
 echo "Downloading MySQL Connector V8.0.24 from Half Serious public S3 Bucket"
 
-# curl --silent https://yadu-public.s3.ca-central-1.amazonaws.com/mysql-connector-java-8.0.24.jar -o ./mysql-connector-java-8.0.24.jar
-curl https://yadu-public.s3.ca-central-1.amazonaws.com/mysql-connector-java-8.0.24.jar -o ./mysql-connector-java-8.0.24.jar
+curl --silent https://yadu-public.s3.ca-central-1.amazonaws.com/mysql-connector-java-8.0.24.jar -o ./mysql-connector-java-8.0.24.jar
 
 popd > /dev/null
