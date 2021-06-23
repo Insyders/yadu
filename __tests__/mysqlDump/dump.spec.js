82443a292a647ab5ae1f2dcab3c64893e5aeb8a6
tommygingras <tommy@halfserious.com>
2021-06-23 10:22:06 -0400

    test: removed hardcoded error

--- __tests__/mysqlDump/dump.spec.js
+++ __tests__/mysqlDump/dump.spec.js
@@ -27,7 +27,6 @@ describe('Test mysqlDump', () => {
 
     expect(() => {
       mysql.DumpDatabase();
-    }).toThrow(`mysqldump: [Warning] Using a password on the command line interface can be insecure.
-mysqldump: Got error: 2002: Can't connect to local MySQL server through socket '/tmp/mysql.sock' (2) when trying to connect`);
+    }).toThrow();
   });
 });
