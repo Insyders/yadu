1928f6c9a9da43795f8631b27344dbd81d1b77cf
tommygingras <tommy@halfserious.com>
2021-08-31 11:10:17 -0400

    test: fixed tests

--- __tests__/utils.spec.js
+++ __tests__/utils.spec.js
@@ -2,6 +2,10 @@ const { defaultZip } = require('../lib/utils');
 
 describe('Test Utils module', () => {
   test('defaultZip function', () => {
-    defaultZip(`${__dirname}/./test_data/_LAMBDA_/lambda_fn`);
+    const fileList = defaultZip(`${__dirname}/./test_data/_LAMBDA_/lambda_fn`);
+
+    console.log(fileList);
+    expect(fileList).toBeDefined();
+    expect(fileList).toEqual('README.md index.js node_modules package.json src');
   });
 });
