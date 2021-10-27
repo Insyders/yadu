bcd1b8e727a3152d8ad3e29b165cc4da87ce3890
tommygingras <tommy@halfserious.com>
2021-10-27 13:38:03 -0400

    test: fixing test for CI

--- __tests__/utils.spec.js
+++ __tests__/utils.spec.js
@@ -1,14 +1,11 @@
-const shell = require('shelljs');
 const { defaultZip } = require('../libs/utils');
 
 describe('Test Utils module', () => {
   test('defaultZip function', () => {
-    shell.cd(`${__dirname}/./test_data/_LAMBDA_/lambda_fn`);
-    shell.exec('npm install');
     const fileList = defaultZip(`${__dirname}/./test_data/_LAMBDA_/lambda_fn`);
 
     console.log(fileList);
     expect(fileList).toBeDefined();
-    expect(fileList).toEqual('README.md index.js node_modules package-lock.json package.json src');
+    expect(fileList).toEqual('README.md index.js package-lock.json package.json src');
   });
 });
