897b3ea8183262f01474adfa06be8d87e9256607
tommygingras <tommy@halfserious.com>
2022-06-28 14:55:21 -0400

    fix: added controls for verbosity

--- libs/logic.js
+++ libs/logic.js
@@ -330,8 +330,19 @@ async function publish(args, config = {}) {
   if (info && info.zipArgs && info.zipArgs.includes('node_modules')) {
     shell.echo('> Install NodeJs production packages'.info);
     shell.rm('-rf', 'node_modules');
+    let verbosity = '';
+    let timing = '';
+    if (process.env.NO_NPM_LOG === '1' || process.env.NO_NPM_LOG === 'true') {
+      verbosity = '--silent';
+    }
+    if (process.env.DEBUG === '1' || process.env.DEBUG === 'true') {
+      verbosity = '--loglevel verbose';
+    }
+    if (process.env.TIMING === '1' || process.env.TIMING === 'true') {
+      timing = '--timing';
+    }
     // added --legacy-peer-deps to support NPM 7, in our case the peerDeps are provided by the layer in AWS
-    shell.exec('npm install --silent --production --legacy-peer-deps');
+    shell.exec(`npm install ${verbosity} ${timing} --production --legacy-peer-deps`);
   } else {
     console.log(`${'----------'.warn}`);
     console.error(`${'[WARN]'.warn}YaDU is missing the zip source directories and files`);
